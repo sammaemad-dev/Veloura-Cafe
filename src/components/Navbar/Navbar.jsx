05332987c92476a595cb18c8c9cf64785ce8e6d4
Sama Emad <sammaemad@gmail.com>
2026-07-24 07:42:41 +0300

    NAvbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,79 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-
       <div className="logo">
         <h2>VELOURA</h2>
         <p>CAFE</p>
       </div>
 
-      <ul className="nav-links">
+      {/* Hamburger */}
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Links */}
+      <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
         </li>
 
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
         </li>
 
         <li>
-          <Link to="/menu">Menu</Link>
+          <Link to="/menu" onClick={() => setMenuOpen(false)}>
+            Menu
+          </Link>
         </li>
 
         <li>
-          <Link to="/reviews">Review</Link>
+          <Link to="/reviews" onClick={() => setMenuOpen(false)}>
+            Review
+          </Link>
         </li>
 
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </Link>
         </li>
+
+        <div className="mobile-auth">
+          <Link
+            to="/login"
+            className="login-btn"
+            onClick={() => setMenuOpen(false)}
+          >
+            Login
+          </Link>
+
+          <Link
+            to="/register"
+            className="register-btn"
+            onClick={() => setMenuOpen(false)}
+          >
+            Register
+          </Link>
+        </div>
       </ul>
 
       <div className="auth-buttons">
-
         <Link to="/login" className="login-btn">
           Login
         </Link>
@@ -41,9 +81,7 @@ function Navbar() {
         <Link to="/register" className="register-btn">
           Register
         </Link>
-
       </div>
-
     </nav>
   );
 }
